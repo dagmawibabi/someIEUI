a2220d95f85b690c27e2e4491356160ec52e095c
dagmawibabi <1babidagi@gmail.com>
2023-08-22 11:05:15 +0300

    done

--- src/components/subtasks.js
+++ src/components/subtasks.js
@@ -10,17 +10,17 @@ export default function Subtasks({addSubtask}) {
     
       return (
         <div className="px-8 bg-[#f2f2f2] flex items-center">
-            <div className="px-8 py-3 border w-full bg-[#F5FAFD] hover:bg-[#edf8ff]"> 
-                <select value={selectedOption} onChange={handleChange} className="w-fit">
-                    <option value="Option 1" className="w-fit">Option 1</option>
-                    <option value="Option 2">Option 2</option>
-                    <option value="Option 3">Option 3</option>
-                </select>
-            </div>
+            <select value={selectedOption} onChange={handleChange} className="px-4 py-3 border w-full bg-[#F5FAFD] hover:bg-[#edf8ff]">
+                <option value="Option 1" className="w-fit">Option 1</option>
+                <option value="Option 2">Option 2</option>
+                <option value="Option 3">Option 3</option>
+            </select>
+            {/* <div className="px-8 py-3 border w-full bg-[#F5FAFD] hover:bg-[#edf8ff]"> 
+            </div> */}
             <div className="px-8 py-3 border w-full bg-[#F5FAFD] hover:bg-[#edf8ff]"> Labor </div>
             <div className="px-8 py-3 border w-full bg-[#F5FAFD] hover:bg-[#edf8ff]"> 3000 </div>
-            <div className="px-5 py-4 border h-full bg-[#F5FAFD] hover:bg-[#edf8ff]">
-                <FaPlus size={18} onClick={() => addSubtask()} />
+            <div className="px-5 py-4 border h-full bg-[#F5FAFD] hover:bg-[#edf8ff]"  onClick={() => addSubtask()} >
+                <FaPlus size={18} />
             </div>
             <div className="px-5 py-4 border  bg-[#F5FAFD] hover:bg-[#edf8ff]">
                 <FaEdit size={18} />
